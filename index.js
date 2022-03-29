2755e4c717350427210b633917b213ced8f89a07
hector <hpulido1021@gmail.com>
2022-03-28 19:56:35 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function superbowlWin(record){
+  function isWinningYear(year){
+      return year.result === "W"
+  }
+  
+const winningYearObject = record.find(isWinningYear)
+
+if (winningYearObject){
+    return winningYearObject.year
+ }
+}
+ 
